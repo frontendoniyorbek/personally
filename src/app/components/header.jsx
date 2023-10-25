9db6dcae26e0b6e31c071fd53dc90774bb4ef331
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-10-25 11:12:03 +0500

    hero responsive

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 	return (
 		<header className='md:py-[18px] py-[28px]'>
 			<div className=' flex items-center container header__container'>
-				<h2 className='hover:opacity-80 active:opacity-60 cursor-pointer md:mr-[60px] sm:mr-[30px] mr-7'>
+				<h2 className='hover:opacity-80 active:opacity-60 cursor-pointer md:mr-[60px] sm:mr-[30px] mr-7 header__title'>
 					<Image className='w-[134] h-[24] object-cover' src={'/logo.svg'} alt='logo' width={134} height={24} />
 				</h2>
 				<ul className='ml-1 flex items-center mr-auto header__list'>
@@ -18,7 +18,7 @@ const Header = () => {
 						</a>
 					</li>
 					<li>
-						<a className='hover:text-red-600 cursor-pointer transition duration-150 ease-out hover:ease-in header__link'>
+						<a className='hover:text-red-600 cursor-pointer transition duration-150 ease-out hover:ease-in header__link whitespace-nowrap'>
 							{t('headerSocil')}
 						</a>
 					</li>
